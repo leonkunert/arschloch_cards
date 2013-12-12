c1e9f164b0c801203fad10ea2133b8481b44af4d
Leon Kunert <leonkunert@me.com>
2013-12-12 23:51:56 +0100

    Changed the way tests are handled. They work now.

--- api/test/test.js
+++ api/test/test.js
@@ -2,36 +2,19 @@
 // Require Assert for assertation
 var assert  = require("assert")
   , request = require("supertest")
-  , app = require("../app.js");
+  , app     = require("../app.js");
 
-describe('Does Testing work', function() {
-    describe('Hello Test', function() {
-        it('It Should Just pass', function() {
-            assert.equal('Hello Test', 'Hello Test');
-        })
-        it('It Should Just Not pass', function() {
-            assert.equal(true, false);
-        })
+describe('GET /', function(){
+    it('respond with json', function(done){
+        request(app)
+          .get('/v1/tables')
+          .expect('Content-Type', /json/, done);
+    });
+
+    it('respond with json', function(done){
+        request(app)
+          .get('/')
+          .expect('Content-Type', "text/html; charset=UTF-8")
+          .expect(200, done);
     })
 });
-
-describe('Correct Errors', function() {
-    describe('Can\'t create Tables with get', function() {
-        it('It Should Return No Player Id given', function() {
-
-            request(app)
-                .post('/v1/add/table/52aa3022afe8fa0000000005/')
-                .set('Content-Type', 'application/json')
-                .expect('Content-Length', '20')
-                .expect('{"Error": "No Player id given"}');
-        });
-
-        it('It Should Return No table Id given', function() {
-
-            request(app)
-                .post('/v1/add/table//52aa3022afe8fa0000000005')
-                .set('Content-Type', 'application/json')
-                .expect('{"Error": "o Table id given"}');
-        })
-    })
-})
