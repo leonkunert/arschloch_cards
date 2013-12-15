c2f0485057435326a200281c651b301bf6149f06
Leon Kunert <leonkunert@me.com>
2013-12-15 22:49:20 +0100

    Added Angular Routes extension and worked a bit on the frontend

--- js/ctrl.js
+++ js/ctrl.js
@@ -1,38 +1,40 @@
-var app = angular.module("app", []);
+var app = angular.module("app", ['ngRoute']);
 
-/*app.config(['$routeProvider', function ($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/', {
-            template: '',
+            templateUrl: 'partials/overview.html',
             controller: 'OverviewCtrl'
         })
         .when('/table/:tableId', {
-            template: '',
+            templateUrl: 'partials/table.html',
             controller: 'TableCtrl'
         })
         .otherwise({
             redirectTo: '/'
         });
-}]);*/
+}]);
 
-app.controller("OverviewCtrl", function ($http) {
-    var app = this;
+app.controller("OverviewCtrl", ['$scope', '$http', function ($scope, $http) {
 
     // Getting all Tables
     $http.get("http://localhost:3003/v1/tables")
         .success(function (data) {
             console.log(data);
-            app.tables = data;
+            $scope.tables = data;
         });
+    $scope.message = "Overview";
 
     // Getting all Players
     $http.get("http://localhost:3003/v1/players")
         .success(function (data) {
             console.log(data);
-            app.players = data;
+            $scope.players = data;
+            console.log($scope);
         });
-});
+}]);
 
-app.controller("TableCtrl", function ($scope, $location, $routeProvider) {
+app.controller("TableCtrl", ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
+    $scope.message = "Table";
 
-});
+}]);
